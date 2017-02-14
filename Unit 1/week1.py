7ee57e5ca4f19c9f418bdd0b463f39bbf5d153e9
dhruv3 <dhruv.mnit@gmail.com>
2017-02-14 00:04:06 -0500

    Updating AND operator

--- Unit 1/week1.py
+++ Unit 1/week1.py
@@ -4,10 +4,10 @@ weight = 60
 user_age = int(input("Enter age"))
 user_weight = int(input("Enter weight"))
 
-if (age > user_age & weight > user_weight):
+if (age > user_age and weight > user_weight):
     print("Both less")
     
-elif (age < user_age & weight < user_weight):
+elif (age < user_age and weight < user_weight):
     print("Both greater")
     
 else:
