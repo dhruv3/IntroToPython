9ca1f6522e0c41c67e0d843fceecccaa684ce00b
dhruv3 <dhruv.mnit@gmail.com>
2017-03-28 19:14:20 -0400

    Updating code

--- Unit 4/sol2.py
+++ Unit 4/sol2.py
@@ -1,3 +1,5 @@
+import mySearches
+
 infile = 'rands.txt'
 
 f = open(infile, 'r')
@@ -12,3 +14,16 @@ intList=[]
 for i in dataList:
     intList.append((int)(i))
 intList.sort()
+
+
+print("Searching for 78700")
+print("Binary search returned index:", mySearches.bsearch(78700, intList))
+print("Linear Search returned index:", mySearches.lsearch(78700, intList))
+
+print("Searching for 3333")
+print("Binary search returned index:", mySearches.bsearch(3333, intList))
+print("Linear Search returned index:", mySearches.lsearch(3333, intList))
+
+print("Searching for 1118")
+print("Binary search returned index:", mySearches.bsearch(1118, intList))
+print("Linear Search returned index:", mySearches.lsearch(1118, intList))
